a95cfe18f3298676b4fef72a75cee418b92ca8f9
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-05-17 18:22:13 +0200

    Add prop-types

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
     render() {
         return  (
             <section className={styles.component}>
-                    <Hero />
+                    <Hero titleText={this.props.title} />
+                    <div className={styles.description}>
+                        {this.props.children}
+                    </div>
             </section>
         )
     }

1e12fbb09de0d8264aefe8a3c105f01a742c7657
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-05-19 19:06:31 +0200

    Add card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Card.scss';
+
+class Card extends React.Component {
+    render() {
+        return (
+            <section className={styles.component}>
+
+            </section>
+        )
+    }
+}
+
+export default Card;

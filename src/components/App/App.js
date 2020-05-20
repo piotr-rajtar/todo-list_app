9ab8d67345c6ced102e581ea6afd1fd1bdc5893a
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-05-20 20:59:41 +0200

    Remove free spaces

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './App.scss';
 import List from '../List/List';
 import {pageContents, listData} from '../../data/dataStore';
+import Creator from '../Creator/Creator';
 
 class App extends React.Component {
   render() {

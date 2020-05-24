66f23e355a4e1a1e58d7f6ebbb83970dd661a91b
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-05-24 22:49:37 +0200

    Add card adding redux functionality

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
-//import Creator from '../Creator/Creator';
+import Creator from '../Creator/Creator';
 import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon';
 
@@ -11,6 +11,7 @@ class Column extends React.Component {
       title: PropTypes.string.isRequired,
       icon: PropTypes.string,
       cards: PropTypes.array,
+      addCard: PropTypes.func,
     }
 
     static defaultProps = {
@@ -18,7 +19,7 @@ class Column extends React.Component {
     }
 
     render() {
-      const {title, icon, cards} = this.props;
+      const {title, icon, cards, addCard} = this.props;
       return (
         <section className={styles.component}>
           <h3 className={styles.title}>
@@ -27,18 +28,16 @@ class Column extends React.Component {
             </span>
             {title}
           </h3>
+
           <div>
             {cards.map(cardData => (
               <Card key={cardData.id} {...cardData} />
             ))} 
           </div>
-          {
-            /*
+          
           <div>
-            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+            <Creator text={settings.cardCreatorText} action={addCard}/>
           </div>
-          */
-          }
         </section>
       );
     }
